11e8ed0f9b120b07224833573c2bf2376b88519f
DHANUSH247 <23203006@srcas.ac.in>
2024-06-22 11:56:14 +0530

    Create dhanu.java

---
+++ dhanu.java
@@ -0,0 +1,8 @@
+public class Hello{
+public static void main(Strings[]args){
+system.out.println("welcome");
+system.out.println("to");
+system.out.println("Devops");
+system.out.println("class");
+}
+}
